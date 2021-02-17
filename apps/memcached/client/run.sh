13e967a9cff53d260f3e33981126bf87c26f8d5f
Shuang Chen <chenshuang0804@gmail.com>
2021-02-17 12:06:58 -0500

    Update run.sh for memcached clients

--- apps/memcached/client/run.sh
+++ apps/memcached/client/run.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# Launch $THREADS clients. Each client tests for $MEASURE seconds. The dataset consists of $SIZE items, each item size is $VALUE bytes.
-
 THREADS=$1
 MEASURE=$2
 SIZE=$3
@@ -17,20 +15,24 @@ PO=/filer-01/datasets/memcached
 mkdir -p $PP/statistics
 CPU=(4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39)
 
-# If this is the first time to test using mutated, you need to run load_memcache first to load the dataset to the memcached server. However, you don't need to load again later.
+# If this is the first time to test using mutated, you need to run load_memcache first to load the dataset to the memcached server. However, you don't need to load again later unless your memcached server is rebooted.
 #echo "load..."
 #$PP/client/load_memcache -z 4000000 -v 200 128.253.128.65:11000
 
 
+# Launch $THREADS memcached clients (the for loop).
+# Each client is pinned to a specific CPU core to avoid inteference between client threads (taskset).
+# Each client opens $CONN connections to the memcache server, testing for $MEASURE seconds (-s).
+# The dataset consists of $SIZE items (-z), each item size is $VALUE bytes (-v).
+# Request inter-arrivial times follow an exponential distribution (-d norm).
+# Tail latency is dumped periodically (every 100ms in PARTIES), to $PO/real_$ii.txt (-x).
+
 for i in `seq 1 $THREADS`
 do   
-    let k=(TIMES-1)*THREADS+i
+    let k=(TIMES-1)*THREADS+i 
     let b=(i-1)*SIZE
     let ii=i-1
-    taskset -c ${CPU[$i]} nice -n -20 $PP/client/mutated_memcache -r -n $CONN -W 10000 -w 10 -d exp -z $SIZE -Z $b -v $VALUE -s $MEASURE $IP:$PORT $QPS > $PP/statistics/$k.txt &
- #   echo $i
- #   echo ${CPU[$i]}
- #   taskset -c ${CPU[$i]} nice -n -20 $PP/client/mutated_memcache -r -n $CONN -W 10000 -w 5 -d norm -z $SIZE -Z $b -v $VALUE -x $PO/real_$ii.txt -s $MEASURE $IP:$PORT $QPS > $PP/statistics/$k.txt &
+    taskset -c ${CPU[$i]} nice -n -20 $PP/client/mutated_memcache -r -n $CONN -W 10000 -w 5 -d norm -z $SIZE -Z $b -v $VALUE -x $PO/real_$ii.txt -s $MEASURE $IP:$PORT $QPS > $PP/statistics/$k.txt &
     sleep 0.1
 done
 
