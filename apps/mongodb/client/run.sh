c3fed97546aece15bc3ac5149c793b24389831a0
Shuang Chen <chenshuang0804@gmail.com>
2021-02-17 13:27:19 -0500

    Update run.sh for YCSB

--- apps/mongodb/client/run.sh
+++ apps/mongodb/client/run.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-RPS=150
-THREADS=64
-IP=128.253.128.76
-PORT=27000
-PN=/home/sc2682/client/ycsb_bin
+RPS=$1
+THREADS=64 # This number should be set to sufficiently large.
+IP=128.253.128.76 # IP address of the MongoDB server
+PORT=27000  # Port number of the MongoDB server
+PN=/home/sc2682/client/ycsb_bin # Path to the YCSB binary
+
+# Run YCSB with $THREADS threads, on a set of cores (taskset), using the workload description (-P workload), with the target request-per-set (-target $RPS)
 
 taskset -c 4-39 nice -n -20 $PN/bin/ycsb run mongodb -p mongodb.url=mongodb://$IP:$PORT/ycsb -s -P workload -threads $THREADS -target $RPS 
