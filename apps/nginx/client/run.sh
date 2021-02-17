8c8b78964fad526e2543c23d39067184e53b1878
Shuang Chen <chenshuang0804@gmail.com>
2021-02-17 13:21:39 -0500

    Update run.sh of wrk2

--- apps/nginx/client/run.sh
+++ apps/nginx/client/run.sh
@@ -13,22 +13,19 @@ let threads=32/CLIENTS
 let CONN=threads*20
 
 rm $PN/statistics/*
+
+# Launch $CLIENTS NGINX clients (the for loop), i.e., $CLIENTS wrk2 instances.
+# Each client has $threads threads, pinned to a specific set of CPU cores to avoid inteference between client threads (taskset -c $sta-$en). 
+# Each client opens $CONN connections to the NGINX server, testing for $MEASURE seconds (-d).
+# Tail latency is dumped periodically (every 100ms in PARTIES), using -p. You may need to change the wrk2 source code the adjust the frequency.
+# scripts/multiplepaths.lua is used to have different wrk2 instances testing different paths. e.g., if the total dataset consists of 1000 html files, and we have 2 wrk2 instances, than the first instance only requests for the first 500 html files, and the second instance tests the last 500 html files. This step is not necessary, so -s multiplepaths.lua can be removed.
+
 for i in `seq 1 $CLIENTS`
 do
     let ii=i-1
-#    let sta=ii*threads+8
-#    let en=sta+threads-1
-    let sta=8
-    let en=39
-    if [ $i -lt 2 ]
-    then
-        taskset -c $sta-$en nice -n -20 $PN/wrk -c $CONN -p -t $threads -d $MEASURE --latency -s $PN/scripts/multiplepaths$i.lua --timeout 10000 -R $RPS http://$IP:$PORT > $PN/statistics/$i.txt &
-        #taskset -c $sta-$en nice -n -20 $PN/wrk -c $CONN -t $threads -d $MEASURE --latency -s $PN/scripts/multiplepaths$i.lua --timeout 10000 -R $RPS http://$IP:$PORT > $PN/statistics/$i.txt &
-    fi
-    if [ $i -gt 1 ]
-    then
-        taskset -c $sta-$en nice -n -20 $PN/wrk -c $CONN -t $threads -d $MEASURE --latency -s $PN/scripts/multiplepaths$i.lua --timeout 10000 -R $RPS http://$IP:$PORT > $PN/statistics/$i.txt &
-    fi
+    let sta=ii*threads
+    let en=sta+threads-1
+    taskset -c $sta-$en nice -n -20 $PN/wrk -c $CONN -p -t $threads -d $MEASURE --latency -s $PN/scripts/multiplepaths$i.lua --timeout 10000 -R $RPS http://$IP:$PORT > $PN/statistics/$i.txt &
     sleep 0.1
 done
 
